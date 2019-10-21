cf0e1d3f15cbb43f1d8df467f0b8af2af92fda81
Noah Talerman <noahtal@umich.edu>
2019-10-20 20:00:10 -0400

    lifting state up

--- src/index.js
+++ src/index.js
@@ -14,17 +14,29 @@ class Square extends React.Component {
       return (
         <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick({value: 'X'})}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
